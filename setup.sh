ac63666b7684bbf28b3e820543b594fa7a57b456
LukasBluebaum <lukasbluebaumb94@gmail.com>
2024-02-20 20:26:22 +0100

    Update download links

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ mkdir data
 mkdir data/models
 mkdir data/paths
 wget https://nlp.stanford.edu/data/glove.6B.zip
-wget https://groups.uni-paderborn.de/wdqa/causenet/causality-graphs/causenet-precision.jsonl.bz2
-wget https://groups.uni-paderborn.de/wdqa/causenet/causality-graphs/causenet-sample.json
+wget https://zenodo.org/records/3876154/files/causenet-precision.jsonl.bz2
+wget https://zenodo.org/records/3876154/files/causenet-sample.json
 mv glove.6B.zip causenet-precision.jsonl.bz2 causenet-sample.json data/
 
 wget https://www.dropbox.com/scl/fi/y1cn9khbtjj0x5eme237p/models.tar.gz?rlkey=dh35zjtq8g7rskz05e42uopk2&e=1&dl=0
